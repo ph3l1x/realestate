75d5cf5ffb658ede60ff3b918acaaa89122bb662
ph3l1x <rperry@mindimage.net>
2016-12-11 13:31:37 -0700

    updates

--- sites/all/modules/real_angular/js/directives/retsSearchForm.directive.js
+++ sites/all/modules/real_angular/js/directives/retsSearchForm.directive.js
@@ -5,16 +5,13 @@ function retsSearchFormDirective(retsAPI) {
         transclude: true,
         templateUrl: '/sites/all/modules/real_angular/themes/search_form.html',
         link: {},
-     //   scope: { "results" : "=" },
         controller: function($scope, $element) {
             $scope.retsFormChange = function() {
                 retsAPI
                     .get($scope.form)
                     .then(function(result) {
                         $scope.results = result;
-                   //     $scope.photoCount = result.photos.length;
-                    //    console.log("PHOTOS: ", result.photos);
-                        console.log("RESULTS: ", result);
+
                     })
             }
         }
--- sites/all/modules/real_angular/js/directives/retsSearchResults.directive.js
+++ sites/all/modules/real_angular/js/directives/retsSearchResults.directive.js
@@ -1,10 +1,17 @@
-function retsSearchResultsDirective() {
+function retsSearchResultsDirective(retsAPI) {
     return {
         restrict: 'A',
         templateUrl: '/sites/all/modules/real_angular/themes/search_results.html',
         replace: false,
         link: function (scope, element) {
 
+            scope.retsFormChange = function() {
+                retsAPI
+                    .get(scope.form)
+                    .then(function(result) {
+                        scope.results = result.data;
+                    })
+            }
         }
     }
 }
