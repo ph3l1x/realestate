4dd6f614ca1175cef2bca7f75c3004eca66e05c4
ph3l1x <rperry@mindimage.net>
2017-03-05 10:57:04 -0700

    changes 3-5-17 10:56am

---
+++ sites/all/modules/real_blocks/templates/least_expensive.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Contains template for rendering least expensive block.
+ */
+
+
+
+
+$query = "select distinct L_Type_ from Listings";
+$rows = mysqli_query($link, $query);
+
+var_dump($rows);
+
+
+?>
+
+cha cha cha
