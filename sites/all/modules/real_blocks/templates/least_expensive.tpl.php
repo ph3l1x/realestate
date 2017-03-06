d66d752b46fe1e69970bde3b6867e01a6196e376
ph3l1x <rperry@mindimage.net>
2017-03-06 14:47:20 -0700

    changes 3-6-17 2:47pmm

--- sites/all/modules/real_blocks/templates/least_expensive.tpl.php
+++ sites/all/modules/real_blocks/templates/least_expensive.tpl.php
@@ -10,14 +10,20 @@ $results = json_decode($listings);
 kpr($results);
 ?>
 <section class="least_expensive">
-  <button class="left"><</button>
-  <button class="right">></button>
   <div class="least_expensive_container">
+      <button class="left"><</button>
+      <button class="right">></button>
 <?php
 foreach($results as $data) {
+    $checkImage = 'http://rets.mindimage.net/images/' . $data->L_ListingID . '-1.jpg';
+    if(!@getimagesize($checkImage)) {
+        $image = 'sites/default/files/no-image.gif';
+    } else {
+      $image = 'http://rets.mindimage.net/' . $data->L_IMAGES . '-1.jpg&w=420&h=200';
+    }
   ?>
   <div class="least_expensive_slider">
-    <img src="http://rets.mindimage.net/<?php print $data->L_IMAGES ?>-1.jpg&w=100">
+    <img src="<?php print $image; ?>">
   </div>
   <?php
   print $data->L_ListingID;
